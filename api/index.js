752c5af847116e0fefe8af54c6b9da2271d91573
FJosseph <aguadolfrank@gmail.com>
2023-07-15 19:01:27 -0500

    fix:SendMessage

--- api/index.js
+++ api/index.js
@@ -32,17 +32,20 @@ io.on('connection', (socket)=>{
     })
 
 
-    socket.on('client:new-message', async(data)=>{
+    socket.on('client:new-message', (data)=>{
         // Envío de mensajes a un solo usuario
-        const {user, userCurrent, message, conversation_id = null, is_group = false} = data
-        console.log(data);
+        // const {user, userCurrent, message, conversation_id = null, is_group = false} = data
+        // console.log(data);
         // if(!data.hasOwnProperty('conversation_id')){
             // Primer mensaje
-            const response = await sendMessage(user, [user, userCurrent], is_group, conversation_id, message)
-            console.log(response);
+            // const response = await sendMessage(user, [user, userCurrent], is_group, conversation_id, message)
+            // console.log(response);
         // }
-        io.to(userCurrent).emit('server: new-message', response)
+        // Emitir evento en la roomp
+        // socket.to(userCurrent).emit('server: new-message', response)
         // socket.to()
+        const {data_message, users} = data
+        io.to(users.userCurrent).emit('server: new-message', data_message)
     })
 
     socket.off('setup', user=>{
--- client/src/stores/chat-store.js
+++ client/src/stores/chat-store.js
@@ -3,60 +3,79 @@ import { defineStore } from "pinia";
 import { useUserStore } from "./user-store";
 import { computed } from "vue";
 
-const URL = 'http://localhost:3001'
+const URL = "http://localhost:3001";
 
-const storeUser = useUserStore()
-const USER_ID = computed(()=>storeUser.user.user._id)
+const storeUser = useUserStore();
+const USER_ID = computed(() => storeUser.user.user._id);
 
 export const useChatStore = defineStore("chat", {
   state: () => ({
     chat: [],
-    users: []
+    users: [],
   }),
   getters: {
-    getListUsers: (state)=>state.users,
-    getConversations: ()=>storeUser.user.user.conversations_id,
-    getConversationsFormatted: ()=>storeUser.user.user.conversations_id.map(x=>{
-      // const user = 
-      if(x.is_group){
-        return
-      }
-      const {_id, firstname, lastname, username} = x.users_id.find(x=>x._id !== USER_ID.value)
-      return {
-        user_id: _id,
-        firstname, lastname, username,
-        ...x,
-        conversation_id: x._id
-      }
-    })
+    getListUsers: (state) => state.users,
+    getConversations: () => storeUser.user.user.conversations_id,
+    getConversationsFormatted: () =>
+      storeUser.user.user.conversations_id.map((x) => {
+        // const user =
+        if (x.is_group) {
+          return;
+        }
+        const { _id, firstname, lastname, username } = x.users_id.find(
+          (x) => x._id !== USER_ID.value
+        );
+        return {
+          user_id: _id,
+          firstname,
+          lastname,
+          username,
+          ...x,
+          conversation_id: x._id,
+        };
+      }),
   },
   actions: {
-    async sendMessage({ text, image, audio }) {
+    async sendMessage({ user, conversation_id, userCurrent, message }) {
       try {
-
-      } catch (error) {
-        
-      }
-    },
-    async setListUsers(){
-      try {
-        const response = await axios.get(`${URL}/chat/users`)
-        this.users = response.data
-        return
-      } catch (error) {
-        throw new Error(error)
-      }
-    },
-    async setChats(idConversation){
-      try {
-          const response = await axios.get(`${URL}/chat/messages/${idConversation}?id_user=${USER_ID.value}`)
-          // console.log(response.data);
-          this.chat = response.data.messages_user[0].messages_id
-          return
+        if (!conversation_id) {
+          const response = await axios.post(`${URL}/chat/messages`, {
+            members_id: [user, userCurrent],
+            sender_id: user,
+            message,
+          });
+          return response.data;
+        }
+        const response = await axios.post(`${URL}/chat/messages?id_conversation=${conversation_id}`, {
+          sender_id: user,
+            message,
+        })
+        return response.data
       } catch (error) {
         console.log(error.response.data.message);
-        this.chat = []
       }
-    }
+    },
+    async setListUsers() {
+      try {
+        const response = await axios.get(`${URL}/chat/users`);
+        this.users = response.data;
+        return;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    async setChats(idConversation) {
+      try {
+        const response = await axios.get(
+          `${URL}/chat/messages/${idConversation}?id_user=${USER_ID.value}`
+        );
+        // console.log(response.data);
+        this.chat = response.data.messages_user[0].messages_id;
+        return;
+      } catch (error) {
+        console.log(error.response.data.message);
+        this.chat = [];
+      }
+    },
   },
 });
