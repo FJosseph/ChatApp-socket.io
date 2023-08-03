a0f26cf1e6bd5a7d1e328223bb7d430c62f92798
FJosseph <aguadolfrank@gmail.com>
2023-08-02 22:05:06 -0500

    add:MobileDesignHome

--- client/src/components/contacts/ConversationList.vue
+++ client/src/components/contacts/ConversationList.vue
@@ -1,26 +1,45 @@
 <template>
-  <q-list >
+  <q-list>
     <q-item
       v-for="contact in list"
       :key="contact.id"
       class=""
       clickable
       v-ripple
-      @click="(userCurrent = contact) && socket.emit('join chat', contact.conversation_id || contact._id)"
+      @click="
+        (userCurrent = contact) &&
+          socket.emit('join chat', contact.conversation_id || contact._id)
+      "
     >
       <q-item-section avatar>
         <q-avatar>
-          <img :src="contact.avatar || 'https://icons.veryicon.com/png/o/miscellaneous/two-color-icon-library/user-286.png'" />
+          <img
+            :src="
+              contact.avatar ||
+              'https://scontent.fpio1-1.fna.fbcdn.net/v/t1.6435-9/109684207_754153485355615_5752930372679852023_n.png?stp=dst-png_p320x320&_nc_cat=109&ccb=1-7&_nc_sid=8024bb&_nc_ohc=hET2W7-m8hkAX8JGQ7d&_nc_ht=scontent.fpio1-1.fna&oh=00_AfA7t0cr-KoRca_8lmq76mYY3etM3S_VEquq4E4W7Ay0CQ&oe=64F29438'
+            "
+          />
           <!-- <q-badge floating rounded color="green"></q-badge> -->
         </q-avatar>
       </q-item-section>
 
       <q-item-section>
-        <q-item-label>{{ `${contact.fullname}` }}</q-item-label>
-        <!-- <q-item-label>{{ contact.lastname }}</q-item-label> -->
+        <q-item-label lines="1">{{ `${contact.fullname}` }}</q-item-label>
+        <q-item-label caption>
+          <template v-if="contact?.last_message?.sender_id !== contact._id">
+            <q-icon :name="iconCheck[contact?.last_message?.is_check]" :color="iconColor[contact?.last_message?.is_check]"/>
+          </template>
+          <!-- <q-icon name="check" /> -->
+          {{ contact?.last_message?.message.text }}
+        </q-item-label>
         <!-- <q-item-label caption lines="1">{{ contact.email }}</q-item-label> -->
       </q-item-section>
-
+      <!-- <q-item-section side>
+                <q-item-label caption>
+                  {{ conversation.time }}
+                </q-item-label>
+                <q-icon name="keyboard_arrow_down" />
+              </q-item-section> -->
       <q-item-section side top>
         <!-- <q-icon name="chat_bubble" color="grey" /> -->
         <!-- <q-badge rounded color="green" /> -->
@@ -30,12 +49,22 @@
 </template>
 <script setup>
 import { inject } from "vue";
-const socket = inject('socket')
+const iconCheck = {
+  true: 'done_all',
+  false: 'check'
+}
+
+const iconColor = {
+  true: 'blue',
+  false: 'black'
+}
+// Socket.io
+const socket = inject("socket");
 defineProps({
   list: {
     type: Array,
   },
 });
-const userCurrent = inject('user-current')
+const userCurrent = inject("user-current");
 </script>
 <style></style>
--- client/src/components/mobile/IndexPage.vue
+++ client/src/components/mobile/IndexPage.vue
@@ -14,7 +14,12 @@
       </q-toolbar>
       <div class="row q-mb-md" id="search_input">
         <q-icon name="search" class="q-ml-sm" size="1.2em"></q-icon>
-        <input class="q-ml-sm" type="text" placeholder="Search" />
+        <input
+          class="q-ml-sm"
+          v-model="search"
+          type="text"
+          placeholder="Search"
+        />
       </div>
     </div>
     <div class="col-12" id="contacts-list_mobile">
@@ -22,28 +27,34 @@
         <contacts-list :list="listConversations" />
       </q-scroll-area>
     </div>
-    <div class="row q-px-md q-pt-sm" style="width:100%;">
-        <q-tabs
+
+    <div id="footer_mobile" class="row q-px-md q-pt-sm" style="width: 100%">
+      <q-tabs
         active-color="black"
         indicator-color="transparent"
         v-model="tabMenu"
         id="tabs_mobile"
-        class="shadow-2 text-blue-grey-12
-
-"
+        class="shadow-2 text-blue-grey-12"
       >
         <q-tab name="messages" icon="sms" />
         <q-tab name="groups" icon="groups" />
         <q-tab name="setting" icon="settings" />
       </q-tabs>
       <!-- {{ tabMenu }} -->
+      <!-- Button actions -->
+      <div class="row btn-actions">
+          <q-btn style="position: absolute;" round icon="add" color="deep-purple-7
+"></q-btn>
+      </div>
     </div>
+
   </div>
 </template>
 <script setup>
 import ContactsList from "src/components/contacts/ConversationList.vue";
-import { ref } from "vue";
-const tabMenu = ref('messages')
+import { inject, ref } from "vue";
+const tabMenu = ref("messages");
+const search = inject("search");
 defineProps({
   user: { type: Object },
   listConversations: { type: Array },
@@ -95,11 +106,24 @@ input:focus {
   width: 100%;
   height: calc(100% - 186px);
 }
-#tabs_mobile{
-    border-radius: 2em;
-    background: rgb(134 148 165 / 6%);
-    border: none;
-    box-shadow: none;
-    width: 100%;
+
+#footer_mobile {
+  background: transparent;
+}
+
+#tabs_mobile {
+  border-radius: 2em;
+  background: rgb(134 148 165 / 6%);
+  border: none;
+  box-shadow: none;
+  width: 100%;
+}
+
+.btn-actions{
+    display: flex;
+    position: relative;
+    z-index: 10;
+    right: 3em;
+    bottom: 4em;
 }
 </style>
