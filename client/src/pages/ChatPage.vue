27bc5cd271f9975b1933781d4524b3c6a8b9153e
FJosseph <aguadolfrank@gmail.com>
2023-08-02 20:29:34 -0500

    fix:DesignChatPage

--- client/src/pages/ChatPage.vue
+++ client/src/pages/ChatPage.vue
@@ -1,137 +1,130 @@
 <template>
-  <q-card id="chat-container" class="">
-    <q-card-section style="height: 100%" horizontal>
-      <q-card-actions
-        v-if="!showProfile"
-        v-show="expanded"
-        class="q-pa-sm"
-        id="contacts"
-        vertical
-        align="center"
-      >
-        <!-- Head contact -->
-        <q-toolbar class="q-pt-md">
-          <!-- <q-icon v-if="showProfile" name="arrow_back" @click="showProfile = false"></q-icon> -->
-          <q-avatar @click="showProfile = true" style="cursor: pointer">
-            <img
-              :src="
-                user.avatar ||
-                'https://cdn-icons-png.flaticon.com/512/9309/9309495.png'
-              "
-            />
-          </q-avatar>
-
-          <q-toolbar-title>
-            <div>
-              {{ `${user?.firstname} ${$q.screen.lt.sm ? "" : user.lastname}` }}
-            </div>
-            <div style="font-size: 0.6em">{{ user.email }}</div>
-          </q-toolbar-title>
-          <q-btn flat round dense icon="more_vert">
-            <list-options-profile :option-banner="optionsProfile" />
-          </q-btn>
-        </q-toolbar>
-        <div class="q-my-sm q-pa-sm" id="search">
-          <q-input
-            rounded
-            outlined
-            dense
-            v-model="search"
-            placeholder="Busca o inicia una conversación"
-          >
-            <template v-slot:prepend>
-              <q-icon name="search" />
-            </template>
-          </q-input>
-        </div>
-        <!-- Contact List -->
-        <!-- <div class="row justify-between" style="width: 100%;">
-              <div style="display: flex;flex-direction: row;align-items: center;" class="q-mx-md text-overline">CONVERSACIONES</div>
-              <q-btn flat round color="primary" icon="add"/>
-          </div> -->
-        <div id="contacts-list">
-          <q-scroll-area style="height: 100%">
-            <!-- <contacts-list :list="contactList" /> -->
-            <contacts-list :list="listDefinitive" />
-          </q-scroll-area>
-        </div>
-      </q-card-actions>
-
-      <q-slide-transition v-if="showProfile" duration="100" appear hidden>
-        <profile-component :user="user" :qr-code="storeUser.user.token_qr" :toggle="() => (showProfile = false)" />
-      </q-slide-transition>
-
-      <q-separator vertical />
-      <!-- <q-btn
-        v-if="$q.screen.lt.sm && !expanded"
-        color="grey"
-        round
-        flat
-        dense
-        :icon="'keyboard_arrow_right'"
-        @click="expanded = !expanded"
-      /> -->
-
-      <q-card-section v-if="userCurrent" id="chats" class="q-pa-none" vertical>
-        <!-- Head chat -->
-        <div id="chat_header" class="row q-pa-sm">
-          <!-- <div>{{ userCurrent }}</div> -->
-          <q-toolbar class="">
-            <q-avatar class="q-ml-none">
-              <img :src="userCurrent.avatar ?? 'https://cdn-icons-png.flaticon.com/512/149/149071.png'" />
-            </q-avatar>
-            <q-toolbar-title>
-              {{ `${userCurrent.fullname}` }}
-            </q-toolbar-title>
-            <q-btn flat round dense icon="more_vert" />
-          </q-toolbar>
-        </div>
-        <!-- Body Chat -->
-        <chat-component :messages="chats" />
-        <!-- Footer Chat -->
-        <div id="chat_footer" class="row q-pa-sm">
-          <div class="q-mr-sm">
-            <q-btn flat round color="black" icon="mood">
-              <q-menu
-          :offset="[0, 18]"
-          transition-show="jump-down"
-          transition-hide="jump-up"
-          persistent
-          style="background: transparent;border: none;box-shadow: none;height: 21rem;"
+  <div style="width: 100%;height: 100%;display: flex;justify-content: center;align-items: center;">
+    <q-card id="chat-container" v-if="!$q.screen.lt.sm">
+      <q-card-section style="height: 100%" horizontal>
+        <q-card-actions
+          v-if="!showProfile"
+          v-show="expanded"
+          class="q-pa-sm"
+          id="contacts"
+          vertical
+          align="center"
         >
-        <emoji-picker v-on:emoji_click="(data)=>clickEmoji(data)"/>
-      </q-menu>
-            </q-btn>
-          </div>
-          <!-- <div id="footer_message"> -->
-          <q-form @submit="handleSendMessage" id="footer_message">
-            <div style="width: -webkit-fill-available" class="q-mr-sm">
-              <input
-                id="message"
-                type="text"
-                v-model="input.text"
-                placeholder="Write your message"
+          <!-- Head contact -->
+          <q-toolbar class="q-pt-md">
+            <!-- <q-icon v-if="showProfile" name="arrow_back" @click="showProfile = false"></q-icon> -->
+            <q-avatar @click="showProfile = true" style="cursor: pointer">
+              <img
+                :src="
+                  user.avatar ||
+                  'https://cdn-icons-png.flaticon.com/512/9309/9309495.png'
+                "
               />
+            </q-avatar>
+  
+            <q-toolbar-title>
+              <div>
+                {{ `${user?.firstname} ${$q.screen.lt.sm ? "" : user.lastname}` }}
+              </div>
+              <div style="font-size: 0.6em">{{ user.email }}</div>
+            </q-toolbar-title>
+            <q-btn flat round dense icon="more_vert">
+              <list-options-profile :option-banner="optionsProfile" />
+            </q-btn>
+          </q-toolbar>
+          <div class="q-my-sm q-pa-sm" id="search">
+            <q-input
+              rounded
+              outlined
+              dense
+              v-model="search"
+              placeholder="Busca o inicia una conversación"
+            >
+              <template v-slot:prepend>
+                <q-icon name="search" />
+              </template>
+            </q-input>
+          </div>
+          <!-- Contact List -->
+          <!-- <div class="row justify-between" style="width: 100%;">
+                <div style="display: flex;flex-direction: row;align-items: center;" class="q-mx-md text-overline">CONVERSACIONES</div>
+                <q-btn flat round color="primary" icon="add"/>
+            </div> -->
+          <div id="contacts-list">
+            <q-scroll-area style="height: 100%">
+              <!-- <contacts-list :list="contactList" /> -->
+              <contacts-list :list="listDefinitive" />
+            </q-scroll-area>
+          </div>
+        </q-card-actions>
+  
+        <q-slide-transition v-if="showProfile" duration="100" appear hidden>
+          <profile-component :user="user" :qr-code="storeUser.user.token_qr" :toggle="() => (showProfile = false)" />
+        </q-slide-transition>
+  
+        <q-separator vertical />  
+        <q-card-section v-if="userCurrent" id="chats" class="q-pa-none" vertical>
+          <!-- Head chat -->
+          <div id="chat_header" class="row q-pa-sm">
+            <!-- <div>{{ userCurrent }}</div> -->
+            <q-toolbar class="">
+              <q-avatar class="q-ml-none">
+                <img :src="userCurrent.avatar ?? 'https://cdn-icons-png.flaticon.com/512/149/149071.png'" />
+              </q-avatar>
+              <q-toolbar-title>
+                {{ `${userCurrent.fullname}` }}
+              </q-toolbar-title>
+              <q-btn flat round dense icon="more_vert" />
+            </q-toolbar>
+          </div>
+          <!-- Body Chat -->
+          <chat-component :messages="chats" />
+          <!-- Footer Chat -->
+          <div id="chat_footer" class="row q-pa-sm">
+            <div class="q-mr-sm">
+              <q-btn flat round color="black" icon="mood">
+                <q-menu
+            :offset="[0, 18]"
+            transition-show="jump-down"
+            transition-hide="jump-up"
+            persistent
+            style="background: transparent;border: none;box-shadow: none;height: 21rem;"
+          >
+          <emoji-picker v-on:emoji_click="(data)=>clickEmoji(data)"/>
+        </q-menu>
+              </q-btn>
             </div>
-            <q-btn flat round color="black" icon="mic" />
-            <q-btn flat type="submit" round color="black" icon="send" />
-          </q-form>
-          <!-- </div> -->
-          <!-- <q-input rounded standout on-focus="none" v-model="text" placeholder="Write your message" bg-color="blue-grey-1" color="blue-grey-12"/> -->
-        </div>
+            <!-- <div id="footer_message"> -->
+            <q-form @submit="handleSendMessage" id="footer_message">
+              <div style="width: -webkit-fill-available" class="q-mr-sm">
+                <input
+                  id="message"
+                  type="text"
+                  v-model="input.text"
+                  placeholder="Write your message"
+                />
+              </div>
+              <q-btn flat round color="black" icon="mic" />
+              <q-btn flat type="submit" round color="black" icon="send" />
+            </q-form>
+            <!-- </div> -->
+            <!-- <q-input rounded standout on-focus="none" v-model="text" placeholder="Write your message" bg-color="blue-grey-1" color="blue-grey-12"/> -->
+          </div>
+        </q-card-section>
       </q-card-section>
-    </q-card-section>
-    <q-dialog v-model="modalGroup">
-      <form-create-group />
-    </q-dialog>
-    <!-- Add Contact -->
-    <q-dialog v-model="modalAddFriend">
-      <form-add-friend-vue/>
-    </q-dialog>
-    <q-dialog v-model="modalAddFriendLast">
-      <add-friend-resume :add-contact="newContact"/>
-    </q-dialog>
-  </q-card>
+      <q-dialog v-model="modalGroup">
+        <form-create-group />
+      </q-dialog>
+      <!-- Add Contact -->
+      <q-dialog v-model="modalAddFriend">
+        <form-add-friend-vue/>
+      </q-dialog>
+      <q-dialog v-model="modalAddFriendLast">
+        <add-friend-resume :add-contact="newContact"/>
+      </q-dialog>
+    </q-card>
+    <mobile-chat :list-conversations="listDefinitive" :user="user" v-else/>
+  </div>
 </template>
 <script setup>
 import ContactsList from "src/components/contacts/ConversationList.vue";
@@ -146,6 +139,7 @@ import { useUserStore } from "src/stores/user-store";
 import FormAddFriendVue from "../components/friend/FormAddFriend.vue";
 import AddFriendResume from "src/components/friend/AddFriendResume.vue";
 import emojiPicker from "../components/emoji_picker/emojiPicker.vue";
+import MobileChat from "../components/mobile/IndexPage.vue";
 
 const clickEmoji = (emoji)=>{
   // alert(emoji)
@@ -158,6 +152,7 @@ const storeUser = useUserStore();
 
 // Search conversation
 const search = ref("");
+provide('search', search)
 
 // Profile state
 const showProfile = ref(false);
@@ -227,7 +222,7 @@ const input = ref({
   image: "",
   audio: "",
 });
-
+provide('inputChat', input)
 // Socket
 const socket = io("http://localhost:3001");
 provide("socket", socket);
