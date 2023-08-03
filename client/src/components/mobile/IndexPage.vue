847b2cfe293e6fbe2b43dc24aa92c4ce485053ad
FJosseph <aguadolfrank@gmail.com>
2023-08-02 22:57:24 -0500

    fix:MenuTab

--- client/src/components/mobile/IndexPage.vue
+++ client/src/components/mobile/IndexPage.vue
@@ -1,67 +1,95 @@
 <template>
-  <div id="container_mobile_chat" class="q-pt-lg">
-    <div class="q-px-md" id="container_header">
-      <q-toolbar class="q-pb-md q-pl-xs">
-        <q-toolbar-title> Messages </q-toolbar-title>
-        <q-avatar @click="showProfile = true" style="cursor: pointer">
-          <img
-            :src="
-              user.avatar ||
-              'https://cdn-icons-png.flaticon.com/512/9309/9309495.png'
-            "
-          />
-        </q-avatar>
-      </q-toolbar>
-      <div class="row q-mb-md" id="search_input">
-        <q-icon name="search" class="q-ml-sm" size="1.2em"></q-icon>
-        <input
-          class="q-ml-sm"
-          v-model="search"
-          type="text"
-          placeholder="Search"
-        />
-      </div>
-    </div>
-    <div class="col-12" id="contacts-list_mobile">
-      <q-scroll-area style="height: 100%; width: 100%">
-        <contacts-list :list="listConversations" />
-      </q-scroll-area>
-    </div>
+  <div id="container_mobile">
+    <div id="container_mobile_chat" class="q-pt-lg">
+        <!-- Messages View -->
+        <div v-if="tabMenu == 'messages'" id="container_home">
+            <div class="q-px-md" id="container_header">
+              <q-toolbar class="q-pb-md q-pl-xs">
+                <q-toolbar-title> Messages </q-toolbar-title>
+                <q-avatar @click="showProfile = true" style="cursor: pointer">
+                  <img
+                    :src="
+                      user.avatar ||
+                      'https://cdn-icons-png.flaticon.com/512/9309/9309495.png'
+                    "
+                  />
+                </q-avatar>
+              </q-toolbar>
+              <div class="row q-mb-md" id="search_input">
+                <q-icon name="search" class="q-ml-sm" size="1.2em"></q-icon>
+                <input
+                  class="q-ml-sm"
+                  v-model="search"
+                  type="text"
+                  placeholder="Search"
+                />
+              </div>
+            </div>
+            <div class="col-12" id="contacts-list_mobile">
+              <q-scroll-area style="height: 100%; width: 100%">
+                <contacts-list :list="listConversations" />
+              </q-scroll-area>
+            </div>
+        </div>
+        <!-- Group View -->
 
-    <div id="footer_mobile" class="row q-px-md q-pt-sm" style="width: 100%">
-      <q-tabs
-        active-color="black"
-        indicator-color="transparent"
-        v-model="tabMenu"
-        id="tabs_mobile"
-        class="shadow-2 text-blue-grey-12"
-      >
-        <q-tab name="messages" icon="sms" />
-        <q-tab name="groups" icon="groups" />
-        <q-tab name="setting" icon="settings" />
-      </q-tabs>
-      <!-- {{ tabMenu }} -->
-      <!-- Button actions -->
-      <div class="row btn-actions">
-          <q-btn style="position: absolute;" round icon="add" color="deep-purple-7
-"></q-btn>
-      </div>
+        <q-slide-transition v-if="tabMenu == 'groups'" duration="100" appear>
+            <group-component />
+        </q-slide-transition>
+        <q-slide-transition v-if="tabMenu == 'setting'" duration="100" appear>
+            <profile-component :user="user" :qr-code="qrCode" :toggle="() => (showProfile = false)" />
+        </q-slide-transition>
+        <!-- Footer -->
+        <div id="footer_mobile" class="row q-px-md q-pt-sm" style="width: 100%">
+          <q-tabs
+            active-color="black"
+            indicator-color="transparent"
+            v-model="tabMenu"
+            id="tabs_mobile"
+            class="shadow-2 text-blue-grey-12"
+          >
+            <q-tab name="messages" icon="sms" />
+            <q-tab name="groups" icon="groups" />
+            <q-tab name="setting" icon="settings" />
+          </q-tabs>
+          <!-- Button actions -->
+          <div class="row btn-actions">
+            <q-btn
+              style="position: absolute"
+              round
+              icon="add"
+              color="deep-purple-7
+        "
+            ></q-btn>
+          </div>
+        </div>
     </div>
-
+    <!-- <div></div> -->
   </div>
 </template>
 <script setup>
+import ProfileComponent from 'src/components/profile/ProfileComponent.vue'
 import ContactsList from "src/components/contacts/ConversationList.vue";
 import { inject, ref } from "vue";
+import GroupComponent from "./GroupComponent.vue";
 const tabMenu = ref("messages");
 const search = inject("search");
 defineProps({
   user: { type: Object },
   listConversations: { type: Array },
+  qrCode: {type: String},
+  toggle: {type: Function}
 });
 </script>
 <style>
-#container_mobile_chat {
+#container_mobile {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  background: white;
+}
+
+#container_mobile_chat, #container_home {
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -109,6 +137,8 @@ input:focus {
 
 #footer_mobile {
   background: transparent;
+  position: absolute;
+  bottom: 10px;
 }
 
 #tabs_mobile {
@@ -119,11 +149,11 @@ input:focus {
   width: 100%;
 }
 
-.btn-actions{
-    display: flex;
-    position: relative;
-    z-index: 10;
-    right: 3em;
-    bottom: 4em;
+.btn-actions {
+  display: flex;
+  position: relative;
+  z-index: 10;
+  right: 3em;
+  bottom: 4em;
 }
 </style>
